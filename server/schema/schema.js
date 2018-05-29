bc59e917b5bd25267b4204f1aaf525ee84c99156
Nicolas Chabanoles <nicolas.chabanoles@bonitasoft.com>
2018-05-29 16:08:55 +0200

    feat(schema) use GraphQLID as type for id

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -1,7 +1,12 @@
 const graphql = require('graphql');
 const _ = require('lodash');
 
-const {GraphQLObjectType, GraphQLString, GraphQLSchema} = graphql;
+const {
+    GraphQLObjectType,
+    GraphQLString,
+    GraphQLSchema,
+    GraphQLID
+    } = graphql;
 
 // dummy data
 var books = [
@@ -14,7 +19,7 @@ var books = [
 const BookType = new GraphQLObjectType({
     name:'Book',
     fields:()=>({
-       id: {type:GraphQLString},
+       id: {type:GraphQLID},
        name: {type:GraphQLString},
        genre: {type:GraphQLString}
     })
@@ -26,11 +31,11 @@ const RootQuery = new GraphQLObjectType({
         // book find by id
         book:{
             type: BookType,
-            args: {id:{type:GraphQLString}},
+            args: {id:{type:GraphQLID}},
             resolve(parent, args){
                 // code to get data from db...
                 // args.id
-                return _.find(books,{id:args.id});
+                return _.find(books,{id:args.id}); // id is automatically transformed to a string when used in JS
             }
         }
     }
