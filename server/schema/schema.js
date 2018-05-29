58085c02c7075d955ad4593ab165dd46bfdc6180
Nicolas Chabanoles <nicolas.chabanoles@bonitasoft.com>
2018-05-29 16:17:01 +0200

    feat(schema) added authors to schema

--- server/schema/schema.js
+++ server/schema/schema.js
@@ -5,7 +5,8 @@ const {
     GraphQLObjectType,
     GraphQLString,
     GraphQLSchema,
-    GraphQLID
+    GraphQLID,
+    GraphQLInt
     } = graphql;
 
 // dummy data
@@ -14,7 +15,11 @@ var books = [
     {name:'The Final Empire',genre:'Fantasy',id:'2'},
     {name:'The Long Earth',genre:'Sci-Fi',id:'3'}
 ];
-
+var authors = [
+    {name:'Patrick Rothfuss', age:44, id:'1'},
+    {name:'Brandon Sanderson', age:42, id:'2'},
+    {name:'Terry Pratchett', age:66, id:'3'}
+];
 
 const BookType = new GraphQLObjectType({
     name:'Book',
@@ -22,6 +27,15 @@ const BookType = new GraphQLObjectType({
        id: {type:GraphQLID},
        name: {type:GraphQLString},
        genre: {type:GraphQLString}
+    })
+});
+
+const AuthorType = new GraphQLObjectType({
+    name:'Author',
+    fields:()=>({
+        id: {type:GraphQLID},
+        name: {type:GraphQLString},
+        age: {type:GraphQLInt}
     })
 });
 
@@ -36,6 +50,15 @@ const RootQuery = new GraphQLObjectType({
                 // code to get data from db...
                 // args.id
                 return _.find(books,{id:args.id}); // id is automatically transformed to a string when used in JS
+            }
+        },
+        author:{
+            type: AuthorType,
+            args: {id:{type:GraphQLID}},
+            resolve(parent, args){
+                // code to get data from db...
+                // args.id
+                return _.find(authors,{id:args.id}); // id is automatically transformed to a string when used in JS
             }
         }
     }
