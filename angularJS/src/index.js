661b1bf9be2d1558558aa311a57f776c4772c4ea
Nicolas Chabanoles <nicolas.chabanoles@bonitasoft.com>
2018-06-01 14:30:33 +0200

    feat(ng-client) create and list books

---
+++ angularJS/src/index.js
@@ -0,0 +1,73 @@
+import AngularApollo from "angular1-apollo";
+import { ApolloClient } from "apollo-client";
+import { InMemoryCache } from "apollo-cache-inmemory";
+import { HttpLink } from "apollo-link-http";
+
+import angular from "angular";
+
+// Queries
+import {getBooksQuery, getAuthorsQuery, addBookMutation} from './queries/queries';
+
+// Style
+import ngAnimate from 'angular-animate'
+import './index.css';
+
+
+angular.module("app", [AngularApollo, ngAnimate]).config(apolloProvider => {
+    apolloProvider.defaultClient(new ApolloClient({
+        link: new HttpLink({ uri: 'http://localhost:4000/graphql' }),
+        cache: new InMemoryCache()
+    }));
+})
+    .controller('MyController', ['$scope', 'apollo', function ($scope, apollo) {
+
+        var fetchBooks = function() {
+
+            apollo
+                .query({query: getBooksQuery})
+                .then(result => {
+                    $scope.books = result.data.books;
+                    console.log('Book list fetched from server:', $scope.books);
+                });
+        };
+
+        $scope.newBook = {};
+
+        var fetchAuthors = function() {
+            apollo
+                .query({query: getAuthorsQuery})
+                .then(result => {
+                    $scope.authors = result.data.authors;
+                });
+        };
+
+        $scope.submit = function() {
+            apollo
+                .mutate({
+                    mutation: addBookMutation,
+                    variables: {
+                        name: $scope.newBook.name,
+                        genre: $scope.newBook.genre,
+                        authorId: $scope.newBook.author.id
+                    },
+                    update: (proxy, { data: { addBook } }) => { // update cache entry to reflect the mutation
+                        const data = proxy.readQuery({ query: getBooksQuery });
+                        data.books.push(addBook);
+                        proxy.writeQuery({ query: getBooksQuery, data });
+                    }
+                })
+                .then(() => {
+                    $scope.newBook = {};
+                    fetchBooks();
+                });
+        };
+
+        // Init
+        fetchBooks();
+        fetchAuthors();
+
+    }]);
+
+// Manually bootstrap your angular application
+angular.bootstrap(document.getElementById('root'), ['app']);
+
---
+++ angularJS/src/queries/queries.js
@@ -0,0 +1,57 @@
+import gql from "graphql-tag";
+
+const getBooksQuery = gql`
+    {
+        books{
+            name
+            id
+            author{
+                name
+                age
+            }
+        }
+    }
+`;
+
+const getBookQuery = gql`
+    query($id:ID){
+        book(id:$id){
+            id
+            name
+            genre
+            author{
+                id
+                name
+                age
+                books{
+                    name
+                    id
+                }
+            }
+        }
+    }
+`;
+
+const getAuthorsQuery = gql`
+    {
+        authors{
+            name
+            id
+        }
+    }
+`;
+
+const addBookMutation = gql`
+    mutation($name:String!,$genre:String!,$authorId:ID!) {
+        addBook(name:$name,genre:$genre,authorId:$authorId){
+            name
+            id
+            author{
+                name
+                age
+            }
+        }
+    }
+`;
+
+export {getAuthorsQuery, getBooksQuery, addBookMutation, getBookQuery};
